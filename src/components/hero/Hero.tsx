1f35a54fe8bc5d2d20d0c08ed0d92a004cb917ae
Christoffer Lund <chrislund90@gmail.com>
2022-05-20 11:40:50 +0200

    Fix hero button styling

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,81 +1,6 @@
-import { createStyles, Container, Text, Button, Group, useMantineTheme, Anchor } from '@mantine/core';
+import { Container, Text, Button, Group, useMantineTheme, Anchor } from '@mantine/core';
 import { Link } from 'react-router-dom';
-
-const BREAKPOINT = '@media (max-width: 755px)';
-
-const useStyles = createStyles(theme => ({
-  wrapper: {
-    position: 'relative',
-    boxSizing: 'border-box',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-  },
-
-  inner: {
-    position: 'relative',
-    paddingTop: 100,
-    paddingBottom: 120,
-
-    [BREAKPOINT]: {
-      paddingBottom: 80,
-      paddingTop: 80,
-    },
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: 62,
-    fontWeight: 900,
-    lineHeight: 1.1,
-    margin: 0,
-    padding: 0,
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-    [BREAKPOINT]: {
-      fontSize: 42,
-      lineHeight: 1.2,
-    },
-  },
-
-  description: {
-    marginTop: theme.spacing.xl,
-    fontSize: 24,
-
-    [BREAKPOINT]: {
-      fontSize: 18,
-    },
-  },
-
-  controls: {
-    marginTop: theme.spacing.xl * 2,
-
-    [BREAKPOINT]: {
-      marginTop: theme.spacing.xl,
-    },
-  },
-
-  control: {
-    height: 54,
-    paddingLeft: 38,
-    paddingRight: 38,
-
-    [BREAKPOINT]: {
-      height: 54,
-      paddingLeft: 18,
-      paddingRight: 18,
-      flex: 1,
-    },
-  },
-
-  githubControl: {
-    borderWidth: 2,
-    borderColor: theme.colorScheme === 'dark' ? 'transparent' : theme.colors.dark[9],
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : 'transparent',
-
-    '&:hover': {
-      backgroundColor: `${theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0]} !important`,
-    },
-  },
-}));
+import useStyles from './Hero.styles';
 
 export function HeroTitle() {
   const { classes, cx } = useStyles();
@@ -96,12 +21,10 @@ export function HeroTitle() {
           Split one image into multiple images with a perfect seam to create those amazing panoramas on Instagram.
         </Text>
 
-        <Group className={classes.controls}>
-          <Anchor component={Link} to='/split'>
-            <Button size='xl' className={classes.control} variant='gradient' gradient={{ from: '#f6416c', to: '#f86789' }}>
-              Start splitting
-            </Button>
-          </Anchor>
+        <div className={classes.controls}>
+          <Button component={Link} to='/split' size='xl' className={classes.control} variant='gradient' gradient={{ from: '#f6416c', to: '#f86789' }}>
+            Start splitting
+          </Button>
 
           <Button
             component='a'
@@ -112,9 +35,9 @@ export function HeroTitle() {
             color={theme.colorScheme === 'dark' ? 'gray' : 'dark'}
             disabled={true}
           >
-            GitHub (coming soon&trade;)
+            GitHub
           </Button>
-        </Group>
+        </div>
       </Container>
     </div>
   );
