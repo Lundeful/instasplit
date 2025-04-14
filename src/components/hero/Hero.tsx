a77d9c0b72a06490412b63ed50d377e8a2451b39
Christoffer Lund <chrislund90@gmail.com>
2025-04-14 03:16:16 +0200

    Refactor Hero component and update dependencies
    Simplified `Hero` component by removing unused `theme` and adjusting styles. Updated `yarn.lock` with upgraded dependencies

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,27 +1,26 @@
-import { Container, Text, useMantineTheme } from '@mantine/core';
-import { Picker } from '../picker/Picker';
+import {Container, Text} from '@mantine/core';
+import {Picker} from '../picker/Picker';
 import useStyles from './Hero.styles';
 
 export function Hero() {
-  const { classes, cx } = useStyles();
-  const theme = useMantineTheme();
+    const {classes} = useStyles();
 
-  return (
-    <div className={classes.wrapper}>
-      <Container size={700} className={classes.inner}>
-        <h1 className={classes.title}>
-          Use{' '}
-          <Text component='span' variant='gradient' gradient={{ from: '#f6416c', to: '#f86789' }} inherit>
-            InstaSplit
-          </Text>{' '}
-          to show off your work in a whole new way.
-        </h1>
+    return (
+        <div className={classes.wrapper}>
+            <Container size={700} className={classes.inner}>
+                <h1 className={classes.title}>
+                    Use{' '}
+                    <Text component='span' variant='gradient' gradient={{from: '#f6416c', to: '#f86789'}} inherit>
+                        InstaSplit
+                    </Text>{' '}
+                    to show off your work in a whole new way.
+                </h1>
 
-        <Text className={classes.description} color='dimmed'>
-          Split one image into multiple images with a perfect seam and create those amazing panoramas on Instagram.
-        </Text>
-        <Picker />
-      </Container>
-    </div>
-  );
+                <Text className={classes.description} color='dimmed'>
+                    Split one image into multiple images with a perfect seam and create those amazing panoramas on Instagram.
+                </Text>
+                <Picker/>
+            </Container>
+        </div>
+    );
 }
