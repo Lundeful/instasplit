5b07ccda88681545c0ad71927c6674df371c92cc
Christoffer Lund <chrislund90@gmail.com>
2025-11-25 21:25:16 +0100

    Make `PreviewLines` non-interactive by adding `pointer-events-none`

--- src/components/splitter/PreviewLines.tsx
+++ src/components/splitter/PreviewLines.tsx
@@ -1,6 +1,6 @@
 export const PreviewLines = ({numberOfSplits}: { numberOfSplits: number }) => {
     return (
-        <div className="w-full h-full absolute flex justify-evenly items-center">
+        <div className="w-full h-full absolute flex justify-evenly items-center pointer-events-none">
             {[...Array(numberOfSplits - 1)].map((_, i) => (
                 <div key={i} className="w-0 h-full border-r border-dashed border-white"/>
             ))}
